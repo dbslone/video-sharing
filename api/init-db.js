84edb1d6313310253e957658d45a8732070231c9
David Slone <dbslone@gmail.com>
2020-08-25 14:44:40 -0700

    remove seed data

--- api/init-db.js
+++ api/init-db.js
@@ -13,16 +13,10 @@ var pg = require('knex')({
 pg.schema.createTable('videos', (table) => {
     table.uuid('id').primary().defaultTo(pg.raw('uuid_generate_v4()'))
     table.string('title')
+    table.string('folder')
 }).then(() => {
-    pg('videos').insert({ title: 'Big Bunny'}).returning(['id', 'title']).then((rows) => {
-        console.log('\n\n\n============')
-        console.log('Use the following urls to view content in the web browser:')
-        rows.map((row) => {
-            console.log(`${row.title}: http://localhost:5000/watch/${row.id}`)
-        })
-        console.log('============\n\n\n')
-        process.exit(0)
-    })
+    console.log('DB Table "videos" created sucessfully. You may now use the video upload script')
+    process.exit()
 }).catch(e => {
     console.error(e)
     process.exit(1)
