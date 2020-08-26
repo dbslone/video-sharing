55c0aaac2775ca6c9dccb9bf985ff6b7f6724d40
David Slone <dbslone@gmail.com>
2020-08-26 09:18:22 -0700

    Add video and 404 route

--- video-sharing-web/src/App.js
+++ video-sharing-web/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
+import VideoPlayer from './VideoPlayer'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <Router>
+        <Route path="/video/:videoId">
+          <VideoPlayer />
+        </Route>
+        <Route path="/404" exact>
+          <div className="NotFound">
+            <h1>404</h1>
+            <p>Unable to find a video</p>
+          </div>
+        </Route>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
